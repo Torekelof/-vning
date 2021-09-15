d2308ca2eeb197d06f106acbae403866c598da3e
torekelof <ekelof.tor@elev.ga.ntig.se>
2021-09-15 11:03:48 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+
+namespace Övning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+
+
+            Console.Write("Vad heter du?");
+
+        string namn = Console.ReadLine();
+            
+        Console.WriteLine("Hej på dig " + namn);
+
+        Console.Write("vart bor du? ");  
+
+        string ort = Console.ReadLine();
+        Console.WriteLine("vad trevligt att du bor i " + ort);
+
+        Console.Write("Hur gammal är du? ");  
+        string ålder = Console.ReadLine();
+        Console.WriteLine($"Du är {ålder} år");   
+        } 
+    }
+}
